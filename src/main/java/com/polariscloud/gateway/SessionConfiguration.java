9a1a721e5d9f0f46d1714dc042d91757e4c6b7a7
Abdul Haleem <abdalhaleem@outlook.com>
2018-01-10 08:54:55 +0500

    initial

---
+++ src/main/java/com/polariscloud/gateway/SessionConfiguration.java
@@ -0,0 +1,8 @@
+package com.polariscloud.gateway;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@EnableRedisHttpSession
+public class SessionConfiguration extends AbstractHttpSessionApplicationInitializer {
+
+}
